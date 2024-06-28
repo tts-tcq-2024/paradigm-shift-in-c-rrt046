8782c4f141e82ebead29d98a105b9b50813d40c4
Remalli Ravi Teja <106062789+rrt046@users.noreply.github.com>
2024-06-28 21:50:50 +0530

    Update checker.c
    changed SOC type from float to int

--- checker.c
+++ checker.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <assert.h>
 
-int batteryIsOk(float temperature, float soc, float chargeRate) {
+int batteryIsOk(float temperature, int soc, float chargeRate) {
   return (temperature < 0 || temperature > 45) ? (printf("Temperature out of range!\n"), 0) :
            (soc < 20 || soc > 80) ? (printf("State of Charge out of range!\n"), 0) :
            (chargeRate > 0.8) ? (printf("Charge Rate out of range!\n"), 0) : 1;
