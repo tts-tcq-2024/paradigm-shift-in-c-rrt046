befa435fdec8e936861157a059a85f39cd9858db
Remalli Ravi Teja <106062789+rrt046@users.noreply.github.com>
2024-07-02 10:04:20 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -46,6 +46,5 @@ int main() {
     assert(!batteryIsOk(50, 85, 0));
     assert(!batteryIsOk(30, 85, 0));
     assert(!batteryIsOk(25, 70, 0.9));
-    printf("All tests passed!\n");
     return 0;
 }
