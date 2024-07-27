ad90c64fe3c088a5637bc13af2aee043289e8dc1
Remalli Ravi Teja <106062789+rrt046@users.noreply.github.com>
2024-07-27 19:04:40 +0530

    Update battery_warning.c

--- battery_warning.c
+++ battery_warning.c
@@ -1,28 +1,11 @@
-// battery_warning.c
 #include "battery_warning.h"
+#include "battery_check.h"
+#include <stdio.h>  // For printf
 
-#define TEMP_LOW_WARNING 2.25
-#define TEMP_HIGH_WARNING 42.75
-#define SOC_LOW_WARNING 24
-#define SOC_HIGH_WARNING 76
-#define CHARGE_RATE_HIGH_WARNING 0.76
-
-bool isTemperatureLowWarning(float temperature) {
-    return (temperature >= 0 && temperature <= TEMP_LOW_WARNING);
-}
-
-bool isTemperatureHighWarning(float temperature) {
-    return (temperature >= TEMP_HIGH_WARNING && temperature <= 45);
-}
-
-bool isSocLowWarning(float soc) {
-    return (soc >= 20 && soc <= SOC_LOW_WARNING);
-}
-
-bool isSocHighWarning(float soc) {
-    return (soc >= SOC_HIGH_WARNING && soc <= 80);
-}
-
-bool isChargeRateHighWarning(float chargeRate) {
-    return (chargeRate >= CHARGE_RATE_HIGH_WARNING && chargeRate <= 0.8);
+void checkBatteryWarnings(Check warnings[], size_t size) {
+    for (size_t i = 0; i < size; ++i) {
+        if (warnings[i].check(warnings[i].value)) {
+            printf("%s", warnings[i].warningMessage);
+        }
+    }
 }
