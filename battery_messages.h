a6c4b56c6610264ee7fd472e0a777306233dc2b4
Remalli Ravi Teja <106062789+rrt046@users.noreply.github.com>
2024-07-27 19:32:55 +0530

    Delete battery_messages.h

--- battery_messages.h
+++
@@ -1,9 +0,0 @@
-#ifndef BATTERY_MESSAGES_H
-#define BATTERY_MESSAGES_H
-
-#include <stddef.h>  // For size_t
-
-// Function to get messages based on language
-const char* getMessage(const char* key);
-
-#endif // BATTERY_MESSAGES_H
