364c3240b1ade3443c9e6b81a725bb0f674663f2
Remalli Ravi Teja <106062789+rrt046@users.noreply.github.com>
2024-07-27 18:50:40 +0530

    Create battery_messages.h

---
+++ battery_messages.h
@@ -0,0 +1,9 @@
+#ifndef BATTERY_MESSAGES_H
+#define BATTERY_MESSAGES_H
+
+#include "battery_common.h"
+
+// Define message retrieval functions
+const char* getMessage(const char* key);
+
+#endif // BATTERY_MESSAGES_H
