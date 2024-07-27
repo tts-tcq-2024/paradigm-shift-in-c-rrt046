7eda29261edbfc3f8815f19654ff856a08141b33
Remalli Ravi Teja <106062789+rrt046@users.noreply.github.com>
2024-07-27 18:54:52 +0530

    Update battery_common.c

--- battery_common.c
+++ battery_common.c
@@ -1,25 +1,9 @@
+// battery_common.c
 #include "battery_common.h"
-#include <stdio.h>
+#include "battery_messages.h"
+
+Language currentLanguage = LANG_ENGLISH; // Default language
 
 void printMessage(const char *message) {
     printf("%s", message);
-}
-
-int checkBatteryParameters(Check checks[], int numChecks) {
-    for (int i = 0; i < numChecks; ++i) {
-        if (!checks[i].check(checks[i].value)) {
-            printMessage(checks[i].errorMessage);
-            return 0;
-        }
-    }
-    return 1;
-}
-
-int checkBatteryWarnings(Check warnings[], int numWarnings) {
-    for (int i = 0; i < numWarnings; ++i) {
-        if (warnings[i].check(warnings[i].value)) {
-            printMessage(warnings[i].warningMessage);
-        }
-    }
-    return 1;
 }
