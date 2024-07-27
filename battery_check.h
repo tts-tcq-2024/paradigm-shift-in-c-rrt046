cb7958c75843613567512fc81a7ec167807c4898
Remalli Ravi Teja <106062789+rrt046@users.noreply.github.com>
2024-07-27 19:31:21 +0530

    Update battery_check.h

--- battery_check.h
+++ battery_check.h
@@ -1,19 +1,12 @@
+// battery_check.h
 #ifndef BATTERY_CHECK_H
 #define BATTERY_CHECK_H
 
-#include <stdbool.h>
-#include <stddef.h>
+#include "battery_common.h"
 
-// Function prototypes
+// Check functions
 bool isTemperatureInRange(float temperature);
 bool isSocInRange(float soc);
 bool isChargeRateInRange(float chargeRate);
-
-bool batteryIsOk(float temperature, float soc, float chargeRate);
-
-// Define the size of the warning tolerance array
-#define NUM_TOLERANCES 3
-
-extern float warningTolerances[NUM_TOLERANCES];
 
 #endif // BATTERY_CHECK_H
