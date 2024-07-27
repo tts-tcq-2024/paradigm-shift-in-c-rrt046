77f828b9842d671741b7cdc758d46605a5cb89f2
Remalli Ravi Teja <106062789+rrt046@users.noreply.github.com>
2024-07-27 19:21:59 +0530

    Update battery_warning.h

--- battery_warning.h
+++ battery_warning.h
@@ -2,6 +2,7 @@
 #define BATTERY_WARNING_H
 
 #include <stddef.h>  // For size_t
+#include "battery_check.h" // Include this to recognize Check type
 
 // Function declarations
 void checkBatteryWarnings(Check warnings[], size_t size);
