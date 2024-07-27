2107934f7d955b7890e54c5373b65c7ad59fa5e5
Remalli Ravi Teja <106062789+rrt046@users.noreply.github.com>
2024-07-27 19:27:14 +0530

    Update battery_warning.h

--- battery_warning.h
+++ battery_warning.h
@@ -1,10 +1,24 @@
 #ifndef BATTERY_WARNING_H
 #define BATTERY_WARNING_H
 
-#include <stddef.h>  // For size_t
-#include "battery_check.h" // Include this to recognize Check type
+#include <stdbool.h>
+#include <stddef.h>
 
-// Function declarations
+// Structure for warning checks
+typedef struct {
+    bool (*check)(float value);
+    float value;
+    const char* warningMessage;
+} Check;
+
+// Function prototypes
 void checkBatteryWarnings(Check warnings[], size_t size);
+
+// Warning functions
+bool isTemperatureLowWarning(float temperature);
+bool isTemperatureHighWarning(float temperature);
+bool isSocLowWarning(float soc);
+bool isSocHighWarning(float soc);
+bool isChargeRateHighWarning(float chargeRate);
 
 #endif // BATTERY_WARNING_H
