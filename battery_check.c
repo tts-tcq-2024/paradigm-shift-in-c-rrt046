cf1ab936c30fcb93238a497005365668860107c0
Remalli Ravi Teja <106062789+rrt046@users.noreply.github.com>
2024-07-27 18:12:51 +0530

    Update battery_check.c

--- battery_check.c
+++ battery_check.c
@@ -1,28 +1,26 @@
 // battery_check.c
 #include <stdio.h>
 #include "battery_check.h"
+#include "battery_warning.h"
 
 void printMessage(const char *message) {
     printf("%s", message);
 }
 
-int isTemperatureInRange(float temperature) {
-    return (temperature >= 0 && temperature <= 45);
-}
-
-int isSocInRange(float soc) {
-    return (soc >= 20 && soc <= 80);
-}
-
-int isChargeRateInRange(float chargeRate) {
-    return (chargeRate <= 0.8);
-}
-
-int checkBatteryParameters(Check *checks, int numChecks) {
+int checkBatteryParameters(Check checks[], int numChecks) {
     for (int i = 0; i < numChecks; ++i) {
         if (!checks[i].check(checks[i].value)) {
-            printMessage(checks[i].message);
+            printMessage(checks[i].errorMessage);
             return 0;
+        }
+    }
+    return 1;
+}
+
+int checkBatteryWarnings(Check warnings[], int numWarnings) {
+    for (int i = 0; i < numWarnings; ++i) {
+        if (warnings[i].check(warnings[i].value)) {
+            printMessage(warnings[i].warningMessage);
         }
     }
     return 1;
