a39161079f24a5f48d42ef6ed891ea8d52d88cd0
Remalli Ravi Teja <106062789+rrt046@users.noreply.github.com>
2024-07-27 17:42:09 +0530

    Create battery_warning.h

---
+++ battery_warning.h
@@ -0,0 +1,14 @@
+// battery_warning.h
+#ifndef BATTERY_WARNING_H
+#define BATTERY_WARNING_H
+
+#include "battery_common.h"
+
+int isTemperatureLowWarning(float temperature);
+int isTemperatureHighWarning(float temperature);
+int isSocLowWarning(float soc);
+int isSocHighWarning(float soc);
+int isChargeRateHighWarning(float chargeRate);
+void checkBatteryWarnings(Check *warnings, int numWarnings);
+
+#endif // BATTERY_WARNING_H
