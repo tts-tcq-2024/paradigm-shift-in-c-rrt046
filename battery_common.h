6166deee70056276578b63f2e735147d3b314fe6
Remalli Ravi Teja <106062789+rrt046@users.noreply.github.com>
2024-07-27 19:12:34 +0530

    Update battery_common.h

--- battery_common.h
+++ battery_common.h
@@ -5,6 +5,9 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
+#include "battery_messages.h"
+#include "battery_check.h"
+#include "battery_warning.h"
 
 typedef enum {
     LANG_ENGLISH,
