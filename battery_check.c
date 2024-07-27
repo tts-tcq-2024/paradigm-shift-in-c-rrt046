ef3dabf619f1318ce8777f422053ac7aec8ec2ce
Remalli Ravi Teja <106062789+rrt046@users.noreply.github.com>
2024-07-27 17:41:38 +0530

    Create battery_check.c

---
+++ battery_check.c
@@ -0,0 +1,29 @@
+// battery_check.c
+#include <stdio.h>
+#include "battery_check.h"
+
+void printMessage(const char *message) {
+    printf("%s", message);
+}
+
+int isTemperatureInRange(float temperature) {
+    return (temperature >= 0 && temperature <= 45);
+}
+
+int isSocInRange(float soc) {
+    return (soc >= 20 && soc <= 80);
+}
+
+int isChargeRateInRange(float chargeRate) {
+    return (chargeRate <= 0.8);
+}
+
+int checkBatteryParameters(Check *checks, int numChecks) {
+    for (int i = 0; i < numChecks; ++i) {
+        if (!checks[i].check(checks[i].value)) {
+            printMessage(checks[i].message);
+            return 0;
+        }
+    }
+    return 1;
+}
