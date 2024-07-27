7b253dd8acb1d39fd1d81c6b8d494e66669c1aac
Remalli Ravi Teja <106062789+rrt046@users.noreply.github.com>
2024-07-27 18:21:25 +0530

    Update battery_warning.c

--- battery_warning.c
+++ battery_warning.c
@@ -1,33 +1,28 @@
 // battery_warning.c
 #include "battery_warning.h"
 
-// Temperature warning tolerance
 #define TEMP_LOW_WARNING 2.25
 #define TEMP_HIGH_WARNING 42.75
-
-// SoC warning tolerance
 #define SOC_LOW_WARNING 24
 #define SOC_HIGH_WARNING 76
-
-// Charge rate warning tolerance
 #define CHARGE_RATE_HIGH_WARNING 0.76
 
 bool isTemperatureLowWarning(float temperature) {
-    return temperature >= 0 && temperature <= TEMP_LOW_WARNING;
+    return (temperature >= 0 && temperature <= TEMP_LOW_WARNING);
 }
 
 bool isTemperatureHighWarning(float temperature) {
-    return temperature >= TEMP_HIGH_WARNING && temperature <= 45;
+    return (temperature >= TEMP_HIGH_WARNING && temperature <= 45);
 }
 
 bool isSocLowWarning(float soc) {
-    return soc >= 20 && soc <= SOC_LOW_WARNING;
+    return (soc >= 20 && soc <= SOC_LOW_WARNING);
 }
 
 bool isSocHighWarning(float soc) {
-    return soc >= SOC_HIGH_WARNING && soc <= 80;
+    return (soc >= SOC_HIGH_WARNING && soc <= 80);
 }
 
 bool isChargeRateHighWarning(float chargeRate) {
-    return chargeRate >= CHARGE_RATE_HIGH_WARNING && chargeRate <= 0.8;
+    return (chargeRate >= CHARGE_RATE_HIGH_WARNING && chargeRate <= 0.8);
 }
