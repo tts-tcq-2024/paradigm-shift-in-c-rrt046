3b7e94a4656e30637230310a74ff94872d6d5bea
Remalli Ravi Teja <106062789+rrt046@users.noreply.github.com>
2024-07-27 18:56:14 +0530

    Update battery_messages.h

--- battery_messages.h
+++ battery_messages.h
@@ -1,16 +1,9 @@
-// battery_messages.h
 #ifndef BATTERY_MESSAGES_H
 #define BATTERY_MESSAGES_H
 
 #include "battery_common.h"
 
-const char* getTemperatureOutOfRangeMessage();
-const char* getSocOutOfRangeMessage();
-const char* getChargeRateOutOfRangeMessage();
-const char* getTemperatureLowWarningMessage();
-const char* getTemperatureHighWarningMessage();
-const char* getSocLowWarningMessage();
-const char* getSocHighWarningMessage();
-const char* getChargeRateHighWarningMessage();
+// Define message retrieval functions
+const char* getMessage(const char* key);
 
 #endif // BATTERY_MESSAGES_H
