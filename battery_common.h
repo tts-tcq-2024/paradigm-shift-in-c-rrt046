0c20f5e1fb70f133d43f390bac41668a86b0543a
Remalli Ravi Teja <106062789+rrt046@users.noreply.github.com>
2024-07-27 18:14:10 +0530

    Update battery_common.h

--- battery_common.h
+++ battery_common.h
@@ -1,3 +1,4 @@
+// battery_common.h
 #ifndef BATTERY_COMMON_H
 #define BATTERY_COMMON_H
 
@@ -6,10 +7,13 @@ typedef int (*CheckFunc)(float);
 typedef struct {
     CheckFunc check;
     float value;
-    const char *message;
+    const char *errorMessage;
     const char *warningMessage;
 } Check;
 
+// Function prototypes
 void printMessage(const char *message);
+int checkBatteryParameters(Check checks[], int numChecks);
+int checkBatteryWarnings(Check warnings[], int numWarnings);
 
 #endif // BATTERY_COMMON_H
