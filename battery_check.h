c2a3dea0b4490f379cbd657ea5037178f27e7293
Remalli Ravi Teja <106062789+rrt046@users.noreply.github.com>
2024-07-27 18:59:43 +0530

    Update battery_check.h

--- battery_check.h
+++ battery_check.h
@@ -1,12 +1,19 @@
-// battery_check.h
 #ifndef BATTERY_CHECK_H
 #define BATTERY_CHECK_H
 
 #include "battery_common.h"
 
-// Check functions
-bool isTemperatureInRange(float temperature);
-bool isSocInRange(float soc);
-bool isChargeRateInRange(float chargeRate);
+// Define the Check structure
+typedef struct {
+    _Bool (*check)(float);  // Function pointer for the check function
+    float value;            // Value to be checked
+    const char* errorMessage; // Error message for out-of-range conditions
+    const char* warningMessage; // Warning message for approaching limits
+} Check;
+
+// Function declarations
+int batteryIsOk(float temperature, float soc, float chargeRate);
+int checkBatteryParameters(Check checks[], size_t size);
+void checkBatteryWarnings(Check warnings[], size_t size);
 
 #endif // BATTERY_CHECK_H
