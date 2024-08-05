86dae51d0a9c051eca18851cdf43814edcdd3023
Remalli Ravi Teja <106062789+rrt046@users.noreply.github.com>
2024-08-05 18:20:57 +0530

    Update battery_common.h

--- battery_common.h
+++ battery_common.h
@@ -1,11 +1,10 @@
-// battery_common.h
 #ifndef BATTERY_COMMON_H
 #define BATTERY_COMMON_H
 
 #include <stdbool.h> 
 #include "battery_messages.h"
 
-typedef bool (*CheckFunc)(float); // Change CheckFunc to return bool
+typedef bool (*CheckFunc)(float);
 
 typedef struct {
     CheckFunc check;
@@ -15,8 +14,8 @@ typedef struct {
 } Check;
 
 // Function prototypes
-void printMessage(Language lang, const char *message);
-int checkBatteryParameters(Check checks[], int numChecks, Language lang);
-int checkBatteryWarnings(Check warnings[], int numWarnings, Language lang);
+void printMessage(const char *message);
+int checkBatteryParameters(Check checks[], int numChecks);
+int checkBatteryWarnings(Check warnings[], int numWarnings);
 
 #endif // BATTERY_COMMON_H
